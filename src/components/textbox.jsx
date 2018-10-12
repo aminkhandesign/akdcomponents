f361c0b92976822a34bd6d773f00a3a5a5961163
Amin Khan <akhan@aacn.org>
2018-10-12 15:55:04 -0700

    textbox functionality

--- src/components/textbox.jsx
+++ src/components/textbox.jsx
@@ -6,8 +6,8 @@ class Textbox extends Component{
     constructor(props){
         super(props);
         this.state={address:[
-                            {type: "appt",ph:"Please enter house/appt number", value:"",active:true},
-                            {type:"street",ph:"Please enter street",value:"",active:false},
+                            {type: "appt",ph:"Please enter house/appt number", value:"",active:false},
+                            {type:"street",ph:"Please enter street",value:"",active:true},
                             {type:"city",ph:"Enter your city",value:"",active:false},
                             {type:"zip",ph:"Enter ZIP",value:"",active:false}
                             ], 
@@ -20,24 +20,40 @@ class Textbox extends Component{
 
     handleChange=(ev)=>{
             let text = ev.target.value;
-
-            text  = text.charAt(0).toUpperCase() + text.slice(1);
-            
+            text  = text.charAt(0).toUpperCase() + text.slice(1);  
             this.setState({contents:text,error:"",line:0})
         
     }
     changeField = (ev)=>{
         ev.preventDefault();
       let myarr = this.state.address;
-      console.log(myarr[0])
-      let idx = myarr.filter((el,i,arr)=>el.active && i)
 
-      console.log(idx)
-    //   this.setState(prevState=>({address:[idx].active=false}))
-    //   this.setState(prevState=>({address:[idx+1].active=true}))
-    let activeField = this.state.address.find(el=>el.active);
+    let idx = this.state.address.find(el=>el.active);
 
-    console.log("active field:" ,activeField)
+
+    idx = myarr.indexOf(idx);
+    switch (myarr[idx].type){
+        case "appt":
+         this.setState(prevState=>(this.state.payload.num = this.state.contents))  
+         break;
+         case "street":
+         this.setState(prevState=>(this.state.payload.street = this.state.contents))  
+         break;
+         case "appt":
+         this.setState(prevState=>(this.state.payload.num = this.state.contents))  
+         break;
+         case "city":
+         this.setState(prevState=>(this.state.payload.city = this.state.contents))  
+         break;
+         case "zip":
+         this.setState(prevState=>(this.state.payload.zip = this.state.contents))  
+         break;
+    }
+    myarr[idx].active=!myarr[idx].active;
+    myarr[idx!==(myarr.length-1)?idx+1:0].active=true
+    console.log(this.state.address)
+
+    this.setState({contents:""})
 
     }
     handleSubmit=(ev)=>{
@@ -51,7 +67,7 @@ class Textbox extends Component{
         let endPoint = this.props.endPoint
         let fetchData = { 
             method: 'POST', 
-            body: this.state.contents,
+            body: this.state.payload,
             headers: new Headers()
         }
 
@@ -63,7 +79,7 @@ class Textbox extends Component{
         
     }
     render() {
-
+        let ph =  this.state.address.find(el=>el.active).ph
         return(
             <div >   
             <label htmlFor={this.props.boxName}>{this.props.boxName}</label>
@@ -72,8 +88,10 @@ class Textbox extends Component{
                     ref={this.inputRef} 
                     onChange={this.handleChange}
                      className={this.props.boxStyle} 
-                    type="text" value={this.state.contents} placeholder={this.state.address.find(el=>el.active).ph}/>
-            <button onClick={this.changeField} >+</button>
+                    type="text" 
+                    value={this.state.contents} 
+                    placeholder={ph}/>
+            <button onClick={this.changeField} type="submit">+</button>
             <br/>
             <input   type="submit" value="submit" onClick={this.handleSubmit} />
             <div className={this.props.displayStyle}>{this.state.display}</div>
